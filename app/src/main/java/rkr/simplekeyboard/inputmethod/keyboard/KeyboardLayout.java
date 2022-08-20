8c3f19149936d3d90fa9dac028fd122da637d6a8
i-e-b <ieb@me.com>
2022-08-20 17:02:01 +0100

    different mode switcher

--- app/src/main/java/rkr/simplekeyboard/inputmethod/keyboard/KeyboardLayout.java
+++ app/src/main/java/rkr/simplekeyboard/inputmethod/keyboard/KeyboardLayout.java
@@ -21,58 +21,58 @@ public class KeyboardLayout {
     public static final char LOK = '\uE105'; // Lock caps
     public static final char AC1 = '\uE106'; // Accents 1
     public static final char AC2 = '\uE107'; // Accents 2
+    public static final char CHM = '\uE108'; // Mode list
 
-    // TODO - Make switching page a whole page to itself
 
     private static final char[][] sLowerLetters = { // LET
             {'t','c',nul,  'q','h','j',  nul,'b','e'},
             {'d','.',nul,  nul,'u',nul,  nul,',','s'},
-            {nul,nul,nul,  nul,SYM,nul,  nul,nul,nul},
+            {nul,nul,nul,  nul,nul,nul,  nul,nul,nul},
 
             {'\'',nul,nul, ARL,RET,ARR,  nul,nul,'"'},
             {'i','k',nul,  NUM,' ',CAP,  nul,'w','a'},
             {'p',nul,nul,  ARU,BAK,ARD,  nul,nul,'l'},
 
-            {nul,nul,nul,  nul,AC1,nul,  '@',nul,nul},
+            {nul,nul,nul,  nul,CHM,nul,  '@',nul,nul},
             {'x',':',nul,  '!','y','?',  nul,'/','z'},
             {'o','v','-',  'm','n','g',  nul,'f','r'},
     };
     private static final char[][] sUpperLetters = { // CAP
             {'T','C',nul,  'Q','H','J',  nul,'B','E'},
             {'D','.',nul,  nul,'U',nul,  nul,',','S'},
-            {nul,nul,nul,  nul,SYM,nul,  nul,nul,nul},
+            {nul,nul,nul,  nul,nul,nul,  nul,nul,nul},
 
             {'\'',nul,nul, ARL,RET,ARR,  '"',nul,nul},
             {'I','K',nul,  NUM,' ',LOK,  nul,'W','A'},
             {'P',nul,nul,  ARU,BAK,ARD,  nul,nul,'L'},
 
-            {nul,nul,nul,  nul,AC1,nul,  '@',nul,nul},
+            {nul,nul,nul,  nul,CHM,nul,  '@',nul,nul},
             {'X',':',nul,  '!','Y','?',  nul,'/','Z'},
             {'O','V','-',  'M','N','G',  nul,'F','R'},
     };
     private static final char[][] sCapsLockLetters = { // LOK
             {'T','C',nul,  'Q','H','J',  nul,'B','E'},
             {'D','.',nul,  nul,'U',nul,  nul,',','S'},
-            {nul,nul,nul,  nul,SYM,nul,  nul,nul,nul},
+            {nul,nul,nul,  nul,nul,nul,  nul,nul,nul},
 
             {'\'',nul,nul, ARL,RET,ARR,  '"',nul,nul},
             {'I','K',nul,  NUM,' ',LET,  nul,'W','A'},
             {'P',nul,nul,  ARU,BAK,ARD,  nul,nul,'L'},
 
-            {nul,nul,nul,  nul,AC1,nul,  '@',nul,nul},
+            {nul,nul,nul,  nul,CHM,nul,  '@',nul,nul},
             {'X',':',nul,  '!','Y','?',  nul,'/','Z'},
             {'O','V','-',  'M','N','G',  nul,'F','R'},
     };
     private static final char[][] sNumeric = { // NUM
             {'1','=',nul,  nul,'3',nul,  nul,'_','5'},
             {'2',nul,nul,  nul,'4',nul,  nul,nul,'6'},
-            {'#','×','%',  nul,SYM,nul,  '‹','›','÷'},
+            {'#','×','%',  nul,nul,nul,  '‹','›','÷'},
 
             {'~',nul,nul,  ARL,RET,ARR,  nul,nul,nul},
             {'7','.',nul,  CAP,' ',LET,  nul,',','0'},
             {'8',nul,nul,  ARU,BAK,ARD,  nul,nul,'9'},
 
-            {nul,nul,nul,  nul,AC1,nul,  '@',nul,nul},
+            {nul,nul,nul,  nul,nul,nul,  '@',nul,nul},
             {'[','|','&',  ':','/',';',  nul,'\\',']'},
             {'(','{','<',  '-','+','*',  '>','}',')'},
     };
@@ -116,6 +116,19 @@ public class KeyboardLayout {
             {'¼','∕','¾',  '←','Ω','→',  '⅖','⅗','⅘'},
             {'⅛','∛','⅞',  '↙','↓','↘',  '⅐','⅑','⅒'},
     };
+    private static final char[][] sChangeMode = { // CHM
+            {SYM,nul,nul,  nul,AC1,nul,  nul,nul,AC2},
+            {nul,nul,nul,  nul,nul,nul,  nul,nul,nul},
+            {nul,nul,nul,  nul,nul,nul,  nul,nul,nul},
+
+            {nul,nul,nul,  nul,nul,nul,  nul,nul,nul},
+            {LOK,nul,nul,  nul,LET,nul,  nul,nul,CAP},
+            {nul,nul,nul,  nul,nul,nul,  nul,nul,nul},
+
+            {nul,nul,nul,  nul,nul,nul,  nul,nul,nul},
+            {nul,nul,nul,  nul,nul,nul,  nul,nul,nul},
+            {nul,nul,nul,  nul,NUM,nul,  nul,nul,nul},
+    };
 
 
     // Note: we could maybe do a 'Unicode pages' mode that moves the base forward/back and just scrolls through everything
@@ -128,6 +141,8 @@ public class KeyboardLayout {
             case CAP: return sUpperLetters;
             case LOK: return sCapsLockLetters;
             case NUM: return sNumeric;
+
+            case CHM: return sChangeMode;
             case AC1: return sAccents1;
             case AC2: return sAccents2;
             case SYM: return sSymbols;
@@ -174,6 +189,7 @@ public class KeyboardLayout {
             case NUM: return "123";
             case AC1: return "äŋç";
             case AC2: return "ÃΣØ";
+            case CHM: return "mode";
             case BAK: return "⇦";
 
             case ARD: return "↓";
@@ -212,8 +228,8 @@ public class KeyboardLayout {
      * @param yi y index 0..2
      */
     public static char TouchUp(int xi, int yi){
-        if (sQuadrantX < 0) return nul;// || xi < 0 || xi >= 3) return nul;
-        if (sQuadrantY < 0) return nul;// || yi < 0 || yi >= 3) return nul;
+        if (sQuadrantX < 0) return nul;
+        if (sQuadrantY < 0) return nul;
 
         // try limiting rather than rejecting?
         if (xi<0) xi=0; if (xi >=3) xi=2;
